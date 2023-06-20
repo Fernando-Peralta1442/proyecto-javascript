24f54e04bacdb36e8c7bea891e57f1f029f346f8
Fernando <fernando.peralta@colcarcordoba.com.ar>
2023-06-20 20:52:01 -0300

    se crea algoritmo de juego de bingo simple

--- index.js
+++ index.js
@@ -1,65 +1,52 @@
-//-------- proyecto de calculadora ------
-function calcular() {
-    const operacion = prompt("Elige una operación (suma, resta, multiplicacion, division):");
-    const num1 = parseInt(prompt("Ingresa el primer número:"));
-    const num2 = parseInt(prompt("Ingresa el segundo número:"));
+//proyecto de bingo , 2da pre entrega//
 
-    let resultado;
-//-------- Bucle----------
-    while (isNaN(num1) || isNaN(num2)) {
-        alert("Por favor, ingresa números válidos.");
-        num1 = parseInt(prompt("Ingresa el primer número:"));
-        num2 = parseInt(prompt("Ingresa el segundo número:"));
+let numinicial=1,numfinal=75
+let sigue,letra
+do{
+    numero=Math.round(Math.random()*(numinicial-numfinal)+numfinal);
+    if (numero>=1 && numero<=15) letra= "B"
+    if (numero>=16 && numero<=30) letra= "I"
+    if (numero>=31 && numero<=45) letra= "N"
+    if (numero>=46 && numero<=60) letra= "G"
+    if (numero>=61 && numero<=75) letra= "O"
+
+    console.log(letra+numero);
+
+    sigue=prompt("Desea sacar una bolilla? S/N")
+}while(sigue=="S" || sigue=="s")
+
+
+
+// registro de asistencias // no terminado
+
+
+// Definir una función de orden superior para crear el registro de asistencias
+function crearRegistro() {
+    let asistencias = {};
+  
+    return function (alumno) {
+      if (!asistencias[alumno]) {
+        asistencias[alumno] = 1;
+      } else {
+        asistencias[alumno]++;
+      }
+  
+      console.log(`Asistencia registrada para ${alumno}`);
+      console.log(`Número total de asistencias para ${alumno}: ${asistencias[alumno]}`);
+    };
+  }
+  
+  // Crear el registro de asistencias
+  const tomarAsistencia = crearRegistro();
+  
+  // Tomar asistencias para los alumnos usando prompt
+  while (true) {
+    const alumno = prompt('Ingresa el nombre del alumno (o presiona Cancelar para terminar):');
+  
+    if (alumno === null) {
+      break; // Salir del bucle si se presiona Cancelar
     }
-// --------aplicamos switch------
-    switch (operacion) {
-        case "suma":
-            resultado = sumar(num1, num2);
-            break;
-        case "resta":
-            resultado = restar(num1, num2);
-            break;
-        case "multiplicacion":
-            resultado = multiplicar(num1, num2);
-            break;
-        case "division":
-            resultado = dividir(num1, num2);
-            break;
-        default:
-            resultado = "Operación inválida";
-    }
-
-    alert("El resultado es: " + resultado);
-}
-//-------- funciones--------
-function sumar(a, b) {
-    return a + b;
-}
-
-function restar(a, b) {
-    return a - b;
-}
-
-function multiplicar(a, b) {
-    return a * b;
-}
-
-function dividir(a, b) {
-    if (b === 0) {
-        return "Error: No se puede dividir por cero";
-    }
-    return a / b;
-}
-
-calcular();
-console.log(calcular)
-
-//ejemplos con if y else-----
-
-const nombre = prompt("Ingresa tu nombre:");
-
-if (nombre) {
-  console.log("¡Hola, " + nombre + "!");
-} else {
-  console.log("No ingresaste un nombre válido.");
-}
+  
+    tomarAsistencia(alumno);
+  }
+  
